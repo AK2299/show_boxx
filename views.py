d9d8891e59bf878ed71ab424bee8c7da28e1d28d
AK2299 <152827324+AK2299@users.noreply.github.com>
2024-01-28 12:39:31 +0530

    Add files via upload

--- views.py
+++ views.py
@@ -1,70 +1,19 @@
-from django.conf import settings
-from django.contrib.flatpages.models import FlatPage
-from django.contrib.sites.shortcuts import get_current_site
-from django.http import Http404, HttpResponse, HttpResponsePermanentRedirect
-from django.shortcuts import get_object_or_404
-from django.template import loader
-from django.utils.safestring import mark_safe
-from django.views.decorators.csrf import csrf_protect
-
-DEFAULT_TEMPLATE = "flatpages/default.html"
-
-# This view is called from FlatpageFallbackMiddleware.process_response
-# when a 404 is raised, which often means CsrfViewMiddleware.process_view
-# has not been called even if CsrfViewMiddleware is installed. So we need
-# to use @csrf_protect, in case the template needs {% csrf_token %}.
-# However, we can't just wrap this view; if no matching flatpage exists,
-# or a redirect is required for authentication, the 404 needs to be returned
-# without any CSRF checks. Therefore, we only
-# CSRF protect the internal implementation.
+from django.contrib import messages
 
 
-def flatpage(request, url):
+class SuccessMessageMixin:
     """
-    Public interface to the flat page view.
-
-    Models: `flatpages.flatpages`
-    Templates: Uses the template defined by the ``template_name`` field,
-        or :template:`flatpages/default.html` if template_name is not defined.
-    Context:
-        flatpage
-            `flatpages.flatpages` object
+    Add a success message on successful form submission.
     """
-    if not url.startswith("/"):
-        url = "/" + url
-    site_id = get_current_site(request).id
-    try:
-        f = get_object_or_404(FlatPage, url=url, sites=site_id)
-    except Http404:
-        if not url.endswith("/") and settings.APPEND_SLASH:
-            url += "/"
-            f = get_object_or_404(FlatPage, url=url, sites=site_id)
-            return HttpResponsePermanentRedirect("%s/" % request.path)
-        else:
-            raise
-    return render_flatpage(request, f)
 
+    success_message = ""
 
-@csrf_protect
-def render_flatpage(request, f):
-    """
-    Internal interface to the flat page view.
-    """
-    # If registration is required for accessing this page, and the user isn't
-    # logged in, redirect to the login page.
-    if f.registration_required and not request.user.is_authenticated:
-        from django.contrib.auth.views import redirect_to_login
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        success_message = self.get_success_message(form.cleaned_data)
+        if success_message:
+            messages.success(self.request, success_message)
+        return response
 
-        return redirect_to_login(request.path)
-    if f.template_name:
-        template = loader.select_template((f.template_name, DEFAULT_TEMPLATE))
-    else:
-        template = loader.get_template(DEFAULT_TEMPLATE)
-
-    # To avoid having to always use the "|safe" filter in flatpage templates,
-    # mark the title and content as already safe (since they are raw HTML
-    # content in the first place).
-    f.title = mark_safe(f.title)
-    f.content = mark_safe(f.content)
-
-    return HttpResponse(template.render({"flatpage": f}, request))
+    def get_success_message(self, cleaned_data):
+        return self.success_message % cleaned_data
